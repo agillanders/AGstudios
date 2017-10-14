52d71b7fae5886becc0b90ffe6397d874e4d09b2
agillanders <alecgillanders@gmail.com>
2017-10-14 20:18:07 +0100

    First commit

---
+++ AGstudios/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AGstudios.Startup))]
+namespace AGstudios
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
